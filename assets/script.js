17c8c858831cd9f7c51f05d6e82ef1b6101276ab
Léo <leo1.gully@gmail.com>
2023-05-29 23:40:07 +0200

    modification du JS (création de tout les éléments  par JS

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,80 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const carouselImages = document.querySelectorAll(".carousel-images img");
-    const bulletPoints = document.querySelectorAll(".bullet-points .bullet");
-    const carouselTextContent = document.querySelector(".carousel-text p");
-    const prevArrow = document.querySelector(".prev-arrow");
-    const nextArrow = document.querySelector(".next-arrow");
-    let currentIndex = 0;
-    const totalImages = carouselImages.length;
-  
-    // Tableau de textes correspondant à chaque image
-    const textArray = [
-      "Impressions tous formats",
-      "Tirages haute définition grand format",
-      "Grand choix de couleurs",
-      "Autocollants"
-    ];
-  
-    const textSpanArray = [
-      "en boutique et en ligne",
-      "pour vos bureaux et events",
-      "de CMJN aux pantones",
-      "avec découpe laser sur mesure"
-  ];
-  
-    // Affiche l'image et met à jour le texte
-    function showImage(index) {
-      carouselImages.forEach(image => image.style.display = "none");
-      carouselImages[index].style.display = "block";
-      carouselTextContent.innerHTML = textArray[index] + ' <span>' + textSpanArray[index] + '</span>';
+const carouselImagesContainer = document.querySelector(".carousel-images");
+const carouselTextContent = document.querySelector(".carousel-text p");
+const bullets = document.querySelectorAll(".bullet");
+
+const carouselData = [
+  {
+    imageSrc: "./assets/images/slideshow/slide1.jpg",
+    text: "Impressions tous formats",
+    textSpan: "en boutique et en ligne"
+  },
+  {
+    imageSrc: "./assets/images/slideshow/slide2.jpg",
+    text: "Tirages haute définition grand format",
+    textSpan: "pour vos bureaux et events"
+  },
+  {
+    imageSrc: "./assets/images/slideshow/slide3.jpg",
+    text: "Grand choix de couleurs",
+    textSpan: "de CMJN aux pantones"
+  },
+  {
+    imageSrc: "./assets/images/slideshow/slide4.png",
+    text: "Autocollants",
+    textSpan: "avec découpe laser sur mesure"
+  }
+];
+
+let currentIndex = 0;
+
+// Insertion des images dans le carousel
+for (const data of carouselData) {
+  const image = document.createElement("img");
+  image.src = data.imageSrc;
+  image.alt = "Image";
+  carouselImagesContainer.appendChild(image);
+}
+
+const carouselImages = document.querySelectorAll(".carousel-images img");
+const prevButton = document.querySelector(".prev-arrow");
+const nextButton = document.querySelector(".next-arrow");
+
+// Affiche l'image, le texte et met à jour le bullet point
+function showSlide(index) {
+  currentIndex = index;
+
+  for (let i = 0; i < carouselImages.length; i++) {
+    const image = carouselImages[i];
+    const bullet = bullets[i];
+
+    if (i === currentIndex) {
+      image.classList.add("active");
+      bullet.classList.add("active");
+    } else {
+      image.classList.remove("active");
+      bullet.classList.remove("active");
     }
-  
-    // Met à jour le point sélectionné
-    function updateBulletPoint(index) {
-      bulletPoints.forEach(bullet => bullet.classList.remove("active"));
-      bulletPoints[index].classList.add("active");
-    }
-  
-    // Affiche l'image suivante
-    function showNextImage() {
-      currentIndex = (currentIndex + 1) % totalImages;
-      showImage(currentIndex);
-      updateBulletPoint(currentIndex);
-    }
-  
-    // Affiche l'image précédente
-    function showPreviousImage() {
-      currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-      showImage(currentIndex);
-      updateBulletPoint(currentIndex);
-    }
-  
-    // Événement clic pour la flèche suivante
-    nextArrow.addEventListener("click", showNextImage);
-  
-    // Événement clic pour la flèche précédente
-    prevArrow.addEventListener("click", showPreviousImage);
-  
-    // Événement clic pour les bullet points
-    bulletPoints.forEach((bullet, index) => {
-      bullet.addEventListener("click", function() {
-        currentIndex = index;
-        showImage(currentIndex);
-        updateBulletPoint(currentIndex);
-      });
-    });
-  
-    // Affiche la première image et le texte correspondant au chargement de la page
-    showImage(currentIndex);
-    updateBulletPoint(currentIndex);
-  });
-  
+  }
+
+  const { text, textSpan } = carouselData[currentIndex];
+  carouselTextContent.innerHTML = `<p>${text} <span>${textSpan}</span></p>`;
+}
+
+// Affiche l'image suivante
+function showNextSlide() {
+  const nextIndex = (currentIndex + 1) % carouselData.length;
+  showSlide(nextIndex);
+}
+
+// Affiche l'image précédente
+function showPreviousSlide() {
+  const prevIndex = (currentIndex - 1 + carouselData.length) % carouselData.length;
+  showSlide(prevIndex);
+}
+
+// Gestion des événements des boutons
+prevButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+// Affiche la première image au chargement de la page
+showSlide(currentIndex);
